4f9952a6a11a3096333c167695efce417c58f530
VertigoCool99 <83370667+VertigoCool99@users.noreply.github.com>
2023-06-29 14:49:08 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2262,7 +2262,7 @@ function library:init_window(cfg)
                             Text = name,
                             Font = Drawing.Fonts.Plex,
                             Size = 13,
-                            Position = UDim2.new(0, 105, 0, 0),
+                            Position = UDim2.new(0, 155, 0, 0),--Changed here
                             Theme = "Text",
                             ZIndex = 7,
                             Outline = false,
