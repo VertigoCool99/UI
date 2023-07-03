f6e65a8d0be089937b2b04f3b01b57faac92a7a9
VertigoCool99 <83370667+VertigoCool99@users.noreply.github.com>
2023-07-03 15:36:34 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -356,7 +356,7 @@ function library:SetOpen(bool)
                 library.cursor.Color = library.theme.Accent;
                 library.cursor.PointA = Vector2.new(mPos.X, mPos.Y);
                 library.cursor.PointB = Vector2.new(mPos.X + 16, mPos.Y + 4);
-                library.cursor.PointC = Vector2.new(mPos.X + 6, mPos.Y + 16);
+                library.cursor.PointC = Vector2.new(mPos.X + 6, mPos.Y + 14);
                 game:GetService("RunService").RenderStepped:Wait();
             end;
         
